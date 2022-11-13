ca489611295c142a5959e6585086e59f11f1cba5
CorrioneroFederico <corrionerofederico@gmail.com>
2022-11-13 19:45:02 -0300

    Inicio de marcadores

--- src/app/mapas/pages/marcadores/marcadores.component.ts
+++ src/app/mapas/pages/marcadores/marcadores.component.ts
@@ -1,16 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-marcadores',
   templateUrl: './marcadores.component.html',
-  styles: [
-  ]
+  styles: [`
+    .mapa-container {
+      width:100%;
+      height: 100%;
+    }
+  `]
 })
-export class MarcadoresComponent implements OnInit {
+export class MarcadoresComponent implements AfterViewInit {
 
+  @ViewChild('mapa') divMapa!: ElementRef;
+  map!: mapboxgl.Map;
+  zoomLevel: number = 16;
+  coordenadas: [number,number] = [-63.76571692817591,-35.66644878392045];
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    this.map = new mapboxgl.Map({
+      container: this.divMapa.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: this.coordenadas,
+      zoom: this.zoomLevel
+    });
+
+    new mapboxgl.Marker().setLngLat(this.coordenadas).addTo(this.map);
   }
+
+
 
 }
