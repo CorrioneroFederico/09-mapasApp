8b46403fb894b8626a8298f2386da6072cd1ec70
CorrioneroFederico <corrionerofederico@gmail.com>
2022-11-13 19:37:07 -0300

    movimiento

--- src/app/mapas/pages/zoom-range/zoom-range.component.ts
+++ src/app/mapas/pages/zoom-range/zoom-range.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, ViewChild } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
 
 @Component({
@@ -17,25 +17,31 @@ import * as mapboxgl from 'mapbox-gl';
       left: 50px;
       padding: 10px;
       position: fixed;
-      width: 300px;
+      width: 400px;
       z-index: 999;
     }
   `]
 })
-export class ZoomRangeComponent implements AfterViewInit {
+export class ZoomRangeComponent implements AfterViewInit, OnDestroy{
 
   @ViewChild('mapa') divMapa!: ElementRef;
 
   map!: mapboxgl.Map;
   zoomLevel: number = 16;
+  coordenadas: [number,number] = [-63.76571692817591,-35.66644878392045];
 
   constructor() { }
+  ngOnDestroy(): void {
+    this.map.off('move',()=>{});
+    this.map.off('zoom',()=>{});
+    this.map.off('zoomend',()=>{});
+  }
 
   ngAfterViewInit(): void {
     this.map = new mapboxgl.Map({
       container: this.divMapa.nativeElement,
       style: 'mapbox://styles/mapbox/streets-v11',
-      center: [-63.76571692817591,-35.66644878392045],
+      center: this.coordenadas,
       zoom: this.zoomLevel
     });
 
@@ -44,6 +50,12 @@ export class ZoomRangeComponent implements AfterViewInit {
     this.map.on('zoomend' , (ev) => {
       if( this.map.getZoom()>18) this.map.zoomTo(18)
       else if(this.map.getZoom()<6) this.map.zoomTo(6);
+    });
+
+    this.map.on('move' , (event) => {
+      const target = event.target;
+      const {lng,lat} = target.getCenter();
+      this.coordenadas = [lng,lat]
     });
   }
 
