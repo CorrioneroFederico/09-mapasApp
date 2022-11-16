f8c2de33a06055ffdad06a1ff844506913bf0f66
CorrioneroFederico <corrionerofederico@gmail.com>
2022-11-16 01:12:03 -0300

    multiples marcadores y colores

--- src/app/mapas/pages/marcadores/marcadores.component.ts
+++ src/app/mapas/pages/marcadores/marcadores.component.ts
@@ -1,6 +1,10 @@
 import { Component, ElementRef, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
 
+interface Marcadores{
+  color : string;
+  marker: mapboxgl.Marker;
+}
 @Component({
   selector: 'app-marcadores',
   templateUrl: './marcadores.component.html',
@@ -8,6 +12,15 @@ import * as mapboxgl from 'mapbox-gl';
     .mapa-container {
       width:100%;
       height: 100%;
+    }
+    .list-group{
+      position:fixed;
+      top: 20px;
+      right: 20px;
+      z-index: 99;
+    }
+    li{
+      cursor:pointer;
     }
   `]
 })
@@ -17,6 +30,9 @@ export class MarcadoresComponent implements AfterViewInit {
   map!: mapboxgl.Map;
   zoomLevel: number = 16;
   coordenadas: [number,number] = [-63.76571692817591,-35.66644878392045];
+
+  marcadores: Marcadores[] = [];
+
   constructor() { }
 
   ngAfterViewInit(): void {
@@ -27,9 +43,25 @@ export class MarcadoresComponent implements AfterViewInit {
       zoom: this.zoomLevel
     });
 
-    new mapboxgl.Marker().setLngLat(this.coordenadas).addTo(this.map);
+    // new mapboxgl.Marker().setLngLat(this.coordenadas).addTo(this.map);
   }
 
+  agregar(){
+    const color = "#xxxxxx".replace(/x/g, y=>(Math.random()*16|0).toString(16));
 
+    const nuevoMarcador = new mapboxgl.Marker({
+      draggable: true,
+      color
+    })
+      .setLngLat(this.coordenadas)
+      .addTo(this.map);
+    this.marcadores.push({
+      color, marker: nuevoMarcador
+    });
+  }
+
+  ir(){
+
+  }
 
 }
