5db335edbf4192ed6ae610848787a52205698a71
CorrioneroFederico <corrionerofederico@gmail.com>
2022-11-20 02:02:15 -0300

    estructura minimapa e incorporacion

--- src/app/mapas/components/mini-mapa/mini-mapa.component.ts
+++ src/app/mapas/components/mini-mapa/mini-mapa.component.ts
@@ -1,16 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild, ElementRef, Input } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-mini-mapa',
   templateUrl: './mini-mapa.component.html',
-  styles: [
-  ]
+  styles: [`
+    div{
+      width : 100%;
+      height: 150px;
+      margin: 0px
+    }
+  `]
 })
-export class MiniMapaComponent implements OnInit {
+export class MiniMapaComponent implements AfterViewInit {
+
+  @Input() lngLat: [number,number] = [0,0];
+  @ViewChild('mapa') divMapa!: ElementRef;          //! Forma de hacer referencia al div
 
   constructor() { }
 
-  ngOnInit(): void {
+  //? Forma general de construir un mapa
+  ngAfterViewInit(): void {
+    const map = new mapboxgl.Map({
+      container: this.divMapa.nativeElement,        //! Indicar donde estará el mapa
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: this.lngLat,
+      zoom: 15,
+      interactive: false
+    });
+
+    new mapboxgl.Marker()
+                .setLngLat(this.lngLat)
+                .addTo(map);
   }
+
+
 
 }
